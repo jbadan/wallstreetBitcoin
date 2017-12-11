69aae2ffc223b70baab59b38fabffe33d8d5c58b
jbadan <jenna.badanowski@gmail.com>
2017-12-11 11:00:19 -0800

    deleting

--- app.js
+++ app.js
@@ -8,13 +8,10 @@ var bodyParser = require('body-parser');
 
 // Mongoose stuff
 var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/meanImageEditor');
-mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
+mongoose.connect('mongodb://localhost/wallstreetBitcoin');
+// mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 
 var index = require('./routes/index');
-var users = require('./routes/users');
-var auth = require('./routes/auth');
-var image = require('./routes/image');
 
 var app = express();
 
@@ -24,23 +21,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
-app.use(function(req, res, next) {
-  // before every route, attach the flash messages and current user to res.locals
-  res.locals.currentUser = req.user;
-  next();
-});
 
-// app.use('/', index);
-app.use('/users', users);
-app.use('/auth', auth);
-app.use('/image', image);
 
-app.get('*', function(req, res, next) {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+app.use('/', index);
+
+// 
+// app.get('*', function(req, res, next) {
+// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 
 
 // catch 404 and forward to error handler - commented out
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var isLoggedIn = require('../middleware/isLoggedIn');
 
 
 module.exports = router;
