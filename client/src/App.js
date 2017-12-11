623ee61de8db19fddc63b17145fa5caebb33ea29
jbadan <jenna.badanowski@gmail.com>
2017-12-11 11:04:16 -0800

    installing packages

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,6 @@ Link,
 Switch
 } from 'react-router-dom'
 import './App.css';
-import Main from './Main/Main';
-import Navbar from './Navbar';
-import NotFound from './NotFound';
-import Profile from './Profile/Profile';
-import NotLoggedInHome from './NotLoggedInHome';
-import Edit from './Main/Edit';
 import axios from 'axios';
 
 
@@ -19,74 +13,14 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      token: '',
-      user: {}
     }
   }
 
-  liftTokenToState = (data) => {
-    this.setState({token: data.token, user: data.user})
-  }
-
-  logout = ()=> {
-    localStorage.removeItem('mernToken')
-    this.setState({token: '', user: {}})
-  }
-
-
-  componentDidMount = () => {
-    // If there is a token in localStorage
-    var token = localStorage.getItem('mernToken')
-    if (token === 'undefined' || token === null || token === '' || token === undefined) {
-      localStorage.removeItem('mernToken')
-      this.setState({
-        token: '',
-        user: {}
-      })
-    } else {
-      //   Validate the token against the server
-      axios.post('/auth/me/from/token', {
-        token: token
-      }).then(response => {
-        //   Store the token and user
-        localStorage.setItem('mernToken', response.data.token)
-        this.setState({
-          token: response.data.token,
-          user: response.data.user
-        })
-        //   Pass User into child components and display main app
-      }).catch(err => {
-        // Both the JWT and db errors will be caught here
-        console.log(err)
-      })
-    }
-  }
 
   render() {
-    let switchStatement = '';
-    if (Object.keys(this.state.user).length === 0) {
-       switchStatement =
-        <Switch>
-          <Route exact path="/" render={() => <NotLoggedInHome />} />
-          <Route path="*" render={NotFound} status={404} />
-        </Switch>
-    } else {
-       switchStatement =
-        <Switch>
-          <Route exact path="/" render={() => <Main user={this.state.user}
-                                                    lift={this.liftTokenToState}
-                                              />} />
-          <Route path="*" render={NotFound} status={404} />
-        </Switch>
-    }
       return (
         <div>
-        <BrowserRouter>
-          <div>
-          <Navbar user={this.state.user} lift={this.liftTokenToState} logout={this.logout} />
-            {switchStatement}
-          </div>
-        </BrowserRouter>
+        hi!
       </div>
       );
     }
--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import customMuiTheme from './muiTheme';
 
-
-const Index = () => (
-  <MuiThemeProvider muiTheme={getMuiTheme(customMuiTheme)}>
-    <App />
-  </MuiThemeProvider>
-);
-
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
